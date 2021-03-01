c1f4b0b619ae65544b62e9ad233890f5218ad6f2
Kara Cavanaugh <karacav@msn.com>
2021-02-28 20:11:28 -0500

    Numbers 3 and 4

--- SQL1.sql
+++
@@ -1,5 +0,0 @@
--- 1. Query the names of all the Japanese cities in the CITY table. The COUNTRYCODE for Japan is JPN
-SELECT NAME FROM CITY WHERE COUNTRYCODE = 'JPN';
-
--- 2. Query all cities and states from a list of stations
-SELECT CITY, STATE FROM STATION;
